737f597c2765a9032c5c392eef991d654698ae46
Yordan Vladov <jordan0vladov@gmail.com>
2024-11-06 11:33:59 +0200

    Added student task

--- Lectures/Week_6/examples/students_task.sh
+++
@@ -1,7 +0,0 @@
-cat students.csv | tail -n +2 | cut -d, -f1-3 | sort -t, -k 3 -nr | head
-cat students.csv | tail -n +2 | cut -d, -f1-3 | sort -t, -k 3 -n | head -n 1
-cat students.csv | tail -n +2 | cut -d, -f5 | sort | uniq -c
-cat students.csv | tail -n +2 | cut -d, -f5,6 | sort | uniq -c
-
-cat students.csv | tail -n +2 | grep "^[^,]*,[^,]*,6"
-cat students.csv | tail -n +2 | grep "^.*,.*,6"
