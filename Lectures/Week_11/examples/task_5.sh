5a586d0c13b2d858d4cae1c3eadb72498cc8c298
Yordan Vladov <jordan0vladov@gmail.com>
2024-12-23 11:46:05 +0200

    Updated summary solutions

--- Lectures/Week_11/examples/task_5.sh
+++ Lectures/Week_11/examples/task_5.sh
@@ -1,6 +1,6 @@
-cat /usr/share/dict/words | grep -E "^a.*e$"
-cat /usr/share/dict/words | grep -E "^[a-z]{4}$"
-cat /usr/share/dict/words | grep -E "^[a-z]{,6}$"
-cat /usr/share/dict/words | tr -cd "a-z\n" | grep -E "^[a-z]{,6}$"
-cat /usr/share/dict/words | grep -E "^[^aeouiAEOUI]*$"
+cat /usr/share/dict/words | grep -E "^a.*e$" | wc -l
+cat /usr/share/dict/words | grep -E "^[a-z]{4}$" | wc -l
+cat /usr/share/dict/words | grep -E "^[a-z]{,6}$" | wc -l
+cat /usr/share/dict/words | tr -cd "a-z\n" | grep -E "^[a-z]{,6}$" | wc -l
+cat /usr/share/dict/words | grep -E "^[^aeouiAEOUI]*$" | wc -l
 cat /usr/share/dict/words | grep -E ".*[0-9].*[A-Z]$"
